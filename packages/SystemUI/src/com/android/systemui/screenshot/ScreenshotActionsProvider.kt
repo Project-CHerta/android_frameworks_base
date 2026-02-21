1dd64118d65b7be886917f1344c4d7f34bab5fd3
Dhina17 <dhinalogu@gmail.com>
2026-02-21 10:03:16 +0700

    SystemUI: Remove screenshot action chip labels
    With label, it kinda looks messy.
    Icons are descriptive enough to understand the action.

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotActionsProvider.kt
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotActionsProvider.kt
@@ -101,7 +101,7 @@ constructor(
         actionsCallback.provideActionButton(
             ActionButtonAppearance(
                 AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_share),
-                context.resources.getString(R.string.screenshot_share_label),
+                null, // context.resources.getString(R.string.screenshot_share_label),
                 context.resources.getString(R.string.screenshot_share_description),
             ),
             showDuringEntrance = true,
@@ -126,7 +126,7 @@ constructor(
         actionsCallback.provideActionButton(
             ActionButtonAppearance(
                 AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_edit),
-                context.resources.getString(R.string.screenshot_edit_label),
+                null, // context.resources.getString(R.string.screenshot_edit_label),
                 context.resources.getString(R.string.screenshot_edit_description),
             ),
             showDuringEntrance = true,
@@ -149,7 +149,7 @@ constructor(
             actionsCallback.provideActionButton(
                 ActionButtonAppearance(
                     AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_scroll),
-                    context.resources.getString(R.string.screenshot_scroll_label),
+                    null, // context.resources.getString(R.string.screenshot_scroll_label),
                     context.resources.getString(R.string.screenshot_scroll_label),
                 ),
                 showDuringEntrance = true,
